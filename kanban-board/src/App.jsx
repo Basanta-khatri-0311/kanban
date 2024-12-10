19bfb1fbd9b5c5032c7382f0b31c291d453de19c
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-12-10 16:58:58 +0545

    created project folders with files

---
+++ kanban-board/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Kanban from './components/Kanban'
+
+const App = () => {
+  return (
+    <>
+    <Kanban/>
+    </>
+  )
+}
+
+export default App
---
+++ kanban-board/src/components/Kanban.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Kanban = () => {
+  return (
+    <div>
+      <button>Add Column</button>
+    </div>
+  )
+}
+
+export default Kanban
